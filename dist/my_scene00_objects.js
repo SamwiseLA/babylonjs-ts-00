167c3d80e46ce3b8ed926b420bd210db936be49f
SamwiseLA <github@samwiser.com>
2022-07-24 18:08:55 -0700

    House Keeping

--- dist/my_scene00_objects.js
+++
@@ -1,8 +0,0 @@
-import "@babylonjs/loaders/glTF";
-import "babylonjs-loaders";
-export default class MySceneObjects {
-    TestModule() {
-        console.log(`In Module: ${typeof this}`);
-    }
-}
-//# sourceMappingURL=my_scene00_objects.js.map
